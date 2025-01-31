107de0c18db1be96495269af21c9e12c6325d363
Angelina Stavniiciuc <angiestavnk@gmail.com>
2025-01-31 14:52:50 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Ensure this includes all your template files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
